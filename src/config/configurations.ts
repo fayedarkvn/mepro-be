30c4c0e0e4d9ec7b97b63560285e2ca417169a7d
trieu04 <truongquoctrieu04@gmail.com>
2024-09-10 01:19:00 +0700

    fix: config file error

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 const configurations = [];
 
 if (fs.existsSync('config/default.yaml')) {
-  configurations.push(() => yaml.load(fs.readFileSync('config/config.yaml', 'utf8')));
+  configurations.push(() => yaml.load(fs.readFileSync('config/default.yaml', 'utf8')));
 }
 
 if (fs.existsSync(`config/${process.env.NODE_ENV}.yaml`)) {
